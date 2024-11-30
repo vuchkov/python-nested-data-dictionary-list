a26caac331b957599f6fcf6f7e4da186f2f0f2b2
Ilcho Vuchkov <ilchovuchkov@gmail.com>
2024-11-30 09:05:20 +0200

    Create recursive funtion which return the sum of all value values

---
+++ README.md
@@ -0,0 +1,7 @@
+## Python nested data structure example
+
+### Nested list in dictionary and dictionary in list
+
+- Inspect the data structure
+- Use recursive function
+- Return the sum of all "value" values
---
+++ main.py
@@ -0,0 +1,26 @@
+root = {
+    'value': 10,
+    'nodes': [{
+        'value': 20,
+        'nodes': [{
+            'value': 30
+        },{
+            'value': 20,
+            'nodes': [{
+                'value': 25
+            }]
+        }]
+    }]
+}
+
+def sum_root(root):
+    current_sum = 0
+    if 'value' in root:
+        # print(root['value'])
+        current_sum += root['value']
+    if 'nodes' in root:
+        for node in root['nodes']:
+            current_sum += sum_root(node)
+    return current_sum
+
+print(sum_root(root))
